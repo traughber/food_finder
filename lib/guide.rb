9a9724c17fbd26e0e2d7fc74d6020933aea6c9e3
Kris <traughberk@gmail.com>
2014-03-29 07:18:21 -0700

    Initial commit.

---
+++ lib/guide.rb
@@ -0,0 +1,26 @@
+class Guide
+
+  def initialize(path=nil)
+    #locate the restaurant text file at path
+    #or create a new file
+    #exit if create fails
+  end
+
+  def launch!
+    introduction
+    #action loop
+    #  what do you want to do? (list, find, add, quit)
+    #  do that action
+    #repeat until user quits
+    conclusion
+  end
+
+  def introduction
+    puts "\n\n<<< Welcome to the Food Finder >>>\n\n"
+  end
+
+  def conclusion
+    puts "\n<<< Goodbye and Bon Appetit >>>\n\n\n"
+  end
+
+end
